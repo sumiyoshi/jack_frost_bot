405e899d37595869b1145a26b182edb49ca8c46f
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2017-04-13 16:27:24 +0900

    update

--- lib/bot_action/action.ex
+++ lib/bot_action/action.ex
@@ -4,16 +4,12 @@ defmodule BotAction.Action do
 
   use Slack
 
-  def hear("hear?", message, slack) do
-    send_message("I can hear", message.channel, slack)
-  end
+  def hear("hear?", message, slack), do: send_message("I can hear", message.channel, slack)
 
-  def hear(_, _, _), do: :ok
+  def hear(_, message, slack), do: send_message("僕のこと呼んでよ！！", message.channel, slack)
 
-  def respond("respond?", message, slack) do
-     send_message("I can respond", message.channel, slack)
-  end
+  def respond("respond?", message, slack), do: send_message("I can respond", message.channel, slack)
 
-  def respond(_, _, _), do: :ok
+  def respond(_, _, _), do: do: send_message("はーーーーーーい！！", message.channel, slack)
 
 end
