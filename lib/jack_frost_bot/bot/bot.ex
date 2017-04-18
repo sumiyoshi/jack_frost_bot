0738dc8ca6f878f4e34817cb4df8bda25181f134
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2017-04-18 13:37:14 +0900

    更新

--- lib/jack_frost_bot/bot/bot.ex
+++ lib/jack_frost_bot/bot/bot.ex
@@ -16,7 +16,7 @@ defmodule JackFrostBot.Bot do
   def handle_event(message = %{type: "message"}, slack, state) do
     cond do
       String.starts_with?(message.text, "<@#{slack.me.id}> #") ->
-        value = String.replace(trigger(message), "#", "") |> String.split("=")
+        value = String.split(String.replace(trigger(message), "#", ""), "=")
         if (Enum.count(value) == 2) do
           reply(JackFrostBot.Action.memory(Enum.at(value, 0), Enum.at(value, 1)), message.channel, slack)
         end
